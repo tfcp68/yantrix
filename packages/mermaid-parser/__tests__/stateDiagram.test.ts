862162427607a84b17acb650d0fd3e2390f6b293
22sasha <bearbearovich@gmail.com>
2023-09-21 12:08:44 +0300

    update tests for state Diagram v2

--- packages/mermaid-parser/__tests__/stateDiagram.test.ts
+++ packages/mermaid-parser/__tests__/stateDiagram.test.ts
@@ -19,18 +19,19 @@ import {
 } from './fixtures/diagramPatterns.js';
 
 import {
-	emptyStateDiagramExpectedResultString,
-	simpleTransitionExpectedResultString,
-	simpleTransitionCompletedExpectedResultString,
-	simpleTransitionWithCommentsExpectedResultString,
-	stateDiagramWithChoiceExpectedResultString,
-	stateDiagramWithForkExpectedResultString,
-	stateDiagramWithLeftSideNoteExpectedResultString,
-	stateDiagramWithRightSideNoteExpectedResultString,
-	stateDiagramWithChoiceAndNoteExpectedResultString,
-	stateDiagramWithSameActionExpectedResultString,
-	stateDiagramWithNamedStatesExpectedResultString,
-	stateDiagramDoublePathExpectedResultString,
+	stateDiagramWithLoopConditionExpectedResultString,
+	emptyStateDiagramExpectedResult,
+	simpleTransitionExpectedResult,
+	simpleTransitionCompletedExpectedResult,
+	simpleTransitionWithCommentsExpectedResult,
+	stateDiagramWithChoiceExpectedResult,
+	stateDiagramWithForkExpectedResult,
+	stateDiagramWithLeftSideNoteExpectedResult,
+	stateDiagramWithRightSideNoteExpectedResult,
+	stateDiagramWithChoiceAndNoteExpectedResult,
+	stateDiagramWithSameActionExpectedResult,
+	stateDiagramWithNamedStatesExpectedResult,
+	stateDiagramDoublePathExpectedResult,
 } from './fixtures/stateDiagramExpectedResults.js';
 
 describe('State Diagram Parser', () => {
@@ -47,102 +48,119 @@ describe('State Diagram Parser', () => {
 			const diagramText = emptyStateDiagram;
 			const parsedDiagram = await parseStateDiagram(diagramText);
 			const stateDiagram = await createStateDiagram(parsedDiagram);
-			const expectedResult = emptyStateDiagramExpectedResultString;
-			expect(JSON.stringify(stateDiagram)).toEqual(expectedResult);
+			const expectedResult = emptyStateDiagramExpectedResult;
+			expect(JSON.stringify(stateDiagram)).toEqual(
+				JSON.stringify(expectedResult),
+			);
 		});
 		test('simpleTransition', async () => {
 			const diagramText = simpleTransition;
 			const parsedDiagram = await parseStateDiagram(diagramText);
 			const stateDiagram = await createStateDiagram(parsedDiagram);
-			const expectedResult = simpleTransitionExpectedResultString;
-			expect(JSON.stringify(stateDiagram)).toEqual(expectedResult);
+			const expectedResult = simpleTransitionExpectedResult;
+			expect(JSON.stringify(stateDiagram)).toEqual(
+				JSON.stringify(expectedResult),
+			);
 		});
 		test('simpleTransitionCompleted', async () => {
 			const diagramText = simpleTransitionCompleted;
 			const parsedDiagram = await parseStateDiagram(diagramText);
 			const stateDiagram = await createStateDiagram(parsedDiagram);
-			const expectedResult =
-				simpleTransitionCompletedExpectedResultString;
-			expect(JSON.stringify(stateDiagram)).toEqual(expectedResult);
+			const expectedResult = simpleTransitionCompletedExpectedResult;
+			expect(JSON.stringify(stateDiagram)).toEqual(
+				JSON.stringify(expectedResult),
+			);
 		});
 		test('simpleTransitionWithComments', async () => {
 			const diagramText = simpleTransitionWithComments;
 			const parsedDiagram = await parseStateDiagram(diagramText);
 			const stateDiagram = await createStateDiagram(parsedDiagram);
-			const expectedResult =
-				simpleTransitionWithCommentsExpectedResultString;
-			expect(JSON.stringify(stateDiagram)).toEqual(expectedResult);
+			const expectedResult = simpleTransitionWithCommentsExpectedResult;
+			expect(JSON.stringify(stateDiagram)).toEqual(
+				JSON.stringify(expectedResult),
+			);
 		});
 		test('stateDiagramWithChoice', async () => {
 			const diagramText = stateDiagramWithChoice;
 			const parsedDiagram = await parseStateDiagram(diagramText);
 			const stateDiagram = await createStateDiagram(parsedDiagram);
-			const expectedResult = stateDiagramWithChoiceExpectedResultString;
-			expect(JSON.stringify(stateDiagram)).toEqual(expectedResult);
+			const expectedResult = stateDiagramWithChoiceExpectedResult;
+			expect(JSON.stringify(stateDiagram)).toEqual(
+				JSON.stringify(expectedResult),
+			);
 		});
 		test('stateDiagramWithFork', async () => {
 			// forks doesnt supported right now
 			const diagramText = stateDiagramWithFork;
 			const parsedDiagram = await parseStateDiagram(diagramText);
 			const stateDiagram = await createStateDiagram(parsedDiagram);
-			const expectedResult = stateDiagramWithForkExpectedResultString;
-			expect(JSON.stringify(stateDiagram)).toEqual(expectedResult);
+			const expectedResult = stateDiagramWithForkExpectedResult;
+			expect(JSON.stringify(stateDiagram)).toEqual(
+				JSON.stringify(expectedResult),
+			);
 		});
 		test('stateDiagramWithLoopCondition', async () => {
 			const diagramText = stateDiagramWithLoopCondition;
 			const parsedDiagram = await parseStateDiagram(diagramText);
+			const expectedResult =
+				stateDiagramWithLoopConditionExpectedResultString;
 			await expect(
 				createStateDiagram(parsedDiagram),
-			).rejects.toThrowError(
-				'Link to itself from the choice side. D-->D',
-			);
+			).rejects.toThrowError(expectedResult);
 		});
 		test('stateDiagramWithLeftSideNote', async () => {
 			const diagramText = stateDiagramWithLeftSideNote;
 			const parsedDiagram = await parseStateDiagram(diagramText);
 			const stateDiagram = await createStateDiagram(parsedDiagram);
-			const expectedResult =
-				stateDiagramWithLeftSideNoteExpectedResultString;
-			expect(JSON.stringify(stateDiagram)).toEqual(expectedResult);
+			const expectedResult = stateDiagramWithLeftSideNoteExpectedResult;
+			expect(JSON.stringify(stateDiagram)).toEqual(
+				JSON.stringify(expectedResult),
+			);
 		});
 		test('stateDiagramWithRightSideNote', async () => {
 			const diagramText = stateDiagramWithRightSideNote;
 			const parsedDiagram = await parseStateDiagram(diagramText);
 			const stateDiagram = await createStateDiagram(parsedDiagram);
-			const expectedResult =
-				stateDiagramWithRightSideNoteExpectedResultString;
-			expect(JSON.stringify(stateDiagram)).toEqual(expectedResult);
+			const expectedResult = stateDiagramWithRightSideNoteExpectedResult;
+			expect(JSON.stringify(stateDiagram)).toEqual(
+				JSON.stringify(expectedResult),
+			);
 		});
 		test('stateDiagramWithChoiceAndNote', async () => {
 			const diagramText = stateDiagramWithChoiceAndNote;
 			const parsedDiagram = await parseStateDiagram(diagramText);
 			const stateDiagram = await createStateDiagram(parsedDiagram);
-			const expectedResult =
-				stateDiagramWithChoiceAndNoteExpectedResultString;
-			expect(JSON.stringify(stateDiagram)).toEqual(expectedResult);
+			const expectedResult = stateDiagramWithChoiceAndNoteExpectedResult;
+			expect(JSON.stringify(stateDiagram)).toEqual(
+				JSON.stringify(expectedResult),
+			);
 		});
 		test('stateDiagramWithSameAction', async () => {
 			const diagramText = stateDiagramWithSameAction;
 			const parsedDiagram = await parseStateDiagram(diagramText);
 			const stateDiagram = await createStateDiagram(parsedDiagram);
-			const expectedResult =
-				stateDiagramWithSameActionExpectedResultString;
-			expect(JSON.stringify(stateDiagram)).toEqual(expectedResult);
+			const expectedResult = stateDiagramWithSameActionExpectedResult;
+			expect(JSON.stringify(stateDiagram)).toEqual(
+				JSON.stringify(expectedResult),
+			);
 		});
 		test('stateDiagramWithNamedStates', async () => {
 			const diagramText = stateDiagramWithNamedStates;
 			const parsedDiagram = await parseStateDiagram(diagramText);
 			const stateDiagram = await createStateDiagram(parsedDiagram);
-			const expectedResult =
-				stateDiagramWithNamedStatesExpectedResultString;
-			expect(JSON.stringify(stateDiagram)).toEqual(expectedResult);
+			const expectedResult = stateDiagramWithNamedStatesExpectedResult;
+			expect(JSON.stringify(stateDiagram)).toEqual(
+				JSON.stringify(expectedResult),
+			);
 		});
 		test('stateDiagramDoublePath', async () => {
 			const diagramText = stateDiagramDoublePath;
 			const parsedDiagram = await parseStateDiagram(diagramText);
 			const stateDiagram = await createStateDiagram(parsedDiagram);
-			const expectedResult = stateDiagramDoublePathExpectedResultString;
-			expect(JSON.stringify(stateDiagram)).toEqual(expectedResult);
+			const expectedResult = stateDiagramDoublePathExpectedResult;
+			expect(JSON.stringify(stateDiagram)).toEqual(
+				JSON.stringify(expectedResult),
+			);
 		});
 	});
 });
