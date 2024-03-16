052ba3d02974b14f502775c1dbddbaacd82ba921
Noktomezo <noktomezo@duck.com>
2024-03-17 01:44:35 +0300

    chore: some code refactoring

--- packages/codegen/src/utils.ts
+++ packages/codegen/src/utils.ts
@@ -1,27 +1,34 @@
+import type { ITypedObject, ITypedObjectProps } from './types.js';
+
 export const toUpperFirst = (str: string) => {
 	return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
-export const toTypedObjectProps = <T extends {}>(obj: T, name: string) => {
-	const typeName = `T${toUpperFirst(name)}`;
-	const typeGuardName = `isValid${toUpperFirst(name)}`;
-	const objectName = `${name}Obj`;
-	const objectBody = JSON.stringify(obj, null, 2);
+export const toTypedObjectProps = <T>(object: T, objName: string) => {
+	const typeName = `T${toUpperFirst(objName)}`;
+	const typeGuardName = `isValid${toUpperFirst(objName)}`;
+	const name = `${objName}Obj`;
+	const body = JSON.stringify(object, null, 2);
 
 	return {
 		typeName,
 		typeGuardName,
-		objectName,
-		objectBody,
-	};
+		name,
+		body,
+	} as ITypedObjectProps;
 };
 
-export const toTypedObjectBlock = <T extends {}>(obj: T, name: string) => {
+export const toTypedObject = <T>(obj: T, name: string) => {
 	const props = toTypedObjectProps(obj, name);
 
-	return `const ${props.objectName} = ${props.objectBody}
-		type ${props.typeName} = keyof typeof ${props.objectName}
+	const codeBlock = `const ${props.name} = ${props.name}
+		type ${props.typeName} = keyof typeof ${props.name}
 		const ${props.typeGuardName} = (obj: any): obj is ${props.typeName} => {
-			return Object.keys(${props.objectName}).includes(obj);
+			return Object.keys(${props.name}).includes(obj);
 		}`;
+
+	return {
+		...props,
+		codeBlock,
+	} as ITypedObject;
 };
