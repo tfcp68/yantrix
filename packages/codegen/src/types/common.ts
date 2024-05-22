8b1c61ac6104421f8fd35e79730aaa4db8eba911
sin <shulgin.grigoriy@yandex.ru>
2024-05-22 14:10:23 +0300

    chore: megre main + prettier

--- packages/codegen/src/types/common.ts
+++ packages/codegen/src/types/common.ts
@@ -24,9 +24,9 @@ export interface ITypedObject extends ITypedObjectProps {
 }
 
 export interface ICodegen {
-	dictionaries: string[];
-	changeStateHandlers: string[];
-	handlersDict: string[];
+	getDictionaries(): string;
+	getChangeStateHandlers(): string;
+	getHandlers(): string;
 	getClassTemplate(className: string): string;
 	getImports(): string;
 }
