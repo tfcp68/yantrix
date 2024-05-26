b4ef20672c28c0da43d416f021bcc4e552b735ab
Denis Perevertov <arrow8086527denis@gmail.com>
2024-05-26 16:45:09 -0400

    fix: function declarations in keyItem.ts changed from static objects to functions

--- packages/yantrix-parser/src/fixtures/keyItem.ts
+++ packages/yantrix-parser/src/fixtures/keyItem.ts
@@ -29,7 +29,11 @@ const createKeyItemDeclaration = (keyItem?: TKeyItem) => {
 const createContext = (keyItems?: TKeyItems) => {
 	const contextItems = keyItems || [baseKeyItemDeclaration()];
 	return {
-		context: contextItems,
+		contextDescription: [
+			{
+				context: contextItems,
+			},
+		],
 	};
 };
 
@@ -40,9 +44,13 @@ const createContextDescription = (
 ) => {
 	const baseContext = createContext(contextKeyItems);
 	return Map(baseContext)
-		.updateIn(['context'], (items) => contextKeyItems ?? items)
-		.updateIn(['payload'], (items) => payloadKeyItems ?? items)
-		.updateIn(['prevContext'], (items) => prevContextKeyItems ?? items)
+		.updateIn(['contextDescription'], (desc) => {
+			const array = <Array<any>>desc;
+			if (contextKeyItems) array[0].context = contextKeyItems;
+			if (payloadKeyItems) array[0].payload = payloadKeyItems;
+			if (prevContextKeyItems) array[0].prevContext = prevContextKeyItems;
+			return array;
+		})
 		.toJS();
 };
 
@@ -62,12 +70,267 @@ export const withIntegerInitial = getKeyItem(expressions.integer);
 export const withPropertyInitial = getKeyItem(expressions.property);
 
 /// Base function declaration
-const expression = getKeyItem(expressions.function);
-const withString = getKeyItem(getExpression(functions.withStringArgs));
-const withProperty = getKeyItem(getExpression(functions.withPropertyArgs));
-const withInteger = getKeyItem(getExpression(functions.withIntegerArgs));
-const recursive = getKeyItem(getExpression(functions.withRecursiveFunction));
-const multiplyProperty = getKeyItem(getExpression(functions.withMultiplyArgs));
+// const expression = getKeyItem(expressions.function);
+// const withString = getKeyItem(getExpression(functions.withStringArgs));
+// const withProperty = getKeyItem(getExpression(functions.withPropertyArgs));
+// const withInteger = getKeyItem(getExpression(functions.withIntegerArgs));
+// const recursive = getKeyItem(getExpression(functions.withRecursiveFunction));
+// const multiplyProperty = getKeyItem(getExpression(functions.withMultiplyArgs));
+
+const expression = (propertyName?: string, functionName?: string) => {
+	const base = getKeyItem(expressions.function);
+	if (propertyName || functionName) {
+		const map = Map(base);
+		const updated = map
+			.updateIn(
+				['contextDescription', 0, 'context', 0, 'KeyItemDeclaration', 'TargetProperty'],
+				(name) => propertyName ?? name,
+			)
+			.updateIn(
+				[
+					'contextDescription',
+					0,
+					'context',
+					0,
+					'KeyItemDeclaration',
+					'Expression',
+					'FunctionDeclaration',
+					'FunctionName',
+				],
+				(name) => functionName ?? name,
+			);
+		return updated.toJS();
+	} else return base;
+};
+
+const withProperty = (propertyName?: string, functionName?: string, functionPropertyName?: string) => {
+	const base = getKeyItem(getExpression(functions.withPropertyArgs));
+	if (propertyName || functionName || functionPropertyName) {
+		const map = Map(base);
+		const updated = map
+			.updateIn(
+				['contextDescription', 0, 'context', 0, 'KeyItemDeclaration', 'TargetProperty'],
+				(name) => propertyName ?? name,
+			)
+			.updateIn(
+				[
+					'contextDescription',
+					0,
+					'context',
+					0,
+					'KeyItemDeclaration',
+					'Expression',
+					'FunctionDeclaration',
+					'FunctionName',
+				],
+				(name) => functionName ?? name,
+			)
+			.updateIn(
+				[
+					'contextDescription',
+					0,
+					'context',
+					0,
+					'KeyItemDeclaration',
+					'Expression',
+					'FunctionDeclaration',
+					'Arguments',
+					0,
+					'Expression',
+					'FunctionProperty',
+				],
+				(name) => functionPropertyName ?? name,
+			);
+		return updated.toJS();
+	} else return base;
+};
+
+const withString = (propertyName?: string, functionName?: string, stringProperty?: string) => {
+	const base = getKeyItem(getExpression(functions.withStringArgs));
+	if (propertyName || functionName || stringProperty) {
+		const map = Map(base);
+		const updated = map
+			.updateIn(
+				['contextDescription', 0, 'context', 0, 'KeyItemDeclaration', 'TargetProperty'],
+				(name) => propertyName ?? name,
+			)
+			.updateIn(
+				[
+					'contextDescription',
+					0,
+					'context',
+					0,
+					'KeyItemDeclaration',
+					'Expression',
+					'FunctionDeclaration',
+					'FunctionName',
+				],
+				(name) => functionName ?? name,
+			)
+			.updateIn(
+				[
+					'contextDescription',
+					0,
+					'context',
+					0,
+					'KeyItemDeclaration',
+					'Expression',
+					'FunctionDeclaration',
+					'Arguments',
+					0,
+					'StringDeclaration',
+				],
+				(name) => stringProperty?.substring(1, stringProperty.length - 1) ?? name,
+			);
+		return updated.toJS();
+	} else return base;
+};
+
+const withInteger = (propertyName?: string, functionName?: string, integerValue?: number) => {
+	const base = getKeyItem(getExpression(functions.withIntegerArgs));
+	if (propertyName || functionName || integerValue) {
+		const map = Map(base);
+		const updated = map
+			.updateIn(
+				['contextDescription', 0, 'context', 0, 'KeyItemDeclaration', 'TargetProperty'],
+				(name) => propertyName ?? name,
+			)
+			.updateIn(
+				[
+					'contextDescription',
+					0,
+					'context',
+					0,
+					'KeyItemDeclaration',
+					'Expression',
+					'FunctionDeclaration',
+					'FunctionName',
+				],
+				(name) => functionName ?? name,
+			)
+			.updateIn(
+				[
+					'contextDescription',
+					0,
+					'context',
+					0,
+					'KeyItemDeclaration',
+					'Expression',
+					'FunctionDeclaration',
+					'Arguments',
+					0,
+					'NumberDeclaration',
+				],
+				(v) => integerValue ?? v,
+			);
+		return updated.toJS();
+	} else return base;
+};
+
+const recursive = (propertyName?: string, functionName?: string, funcRecursiveName?: string) => {
+	const base = getKeyItem(getExpression(functions.withRecursiveFunction));
+	if (propertyName || functionName || funcRecursiveName) {
+		const map = Map(base);
+		const updated = map
+			.updateIn(
+				['contextDescription', 0, 'context', 0, 'KeyItemDeclaration', 'TargetProperty'],
+				(name) => propertyName ?? name,
+			)
+			.updateIn(
+				[
+					'contextDescription',
+					0,
+					'context',
+					0,
+					'KeyItemDeclaration',
+					'Expression',
+					'FunctionDeclaration',
+					'FunctionName',
+				],
+				(name) => functionName ?? name,
+			)
+			.updateIn(
+				[
+					'contextDescription',
+					0,
+					'context',
+					0,
+					'KeyItemDeclaration',
+					'Expression',
+					'FunctionDeclaration',
+					'Arguments',
+					0,
+					'FunctionDeclaration',
+					'FunctionName',
+				],
+				(name) => funcRecursiveName ?? name,
+			);
+		return updated.toJS();
+	} else return base;
+};
+
+const multiplyProperty = (
+	propertyName?: string,
+	functionName?: string,
+	funcPropertyName1?: string,
+	funcPropertyName2?: string,
+) => {
+	const base = getKeyItem(getExpression(functions.withMultiplyArgs));
+	if (propertyName || functionName || funcPropertyName1 || funcPropertyName2) {
+		const map = Map(base);
+		const updated = map
+			.updateIn(
+				['contextDescription', 0, 'context', 0, 'KeyItemDeclaration', 'TargetProperty'],
+				(name) => propertyName ?? name,
+			)
+			.updateIn(
+				[
+					'contextDescription',
+					0,
+					'context',
+					0,
+					'KeyItemDeclaration',
+					'Expression',
+					'FunctionDeclaration',
+					'FunctionName',
+				],
+				(name) => functionName ?? name,
+			)
+			.updateIn(
+				[
+					'contextDescription',
+					0,
+					'context',
+					0,
+					'KeyItemDeclaration',
+					'Expression',
+					'FunctionDeclaration',
+					'Arguments',
+					0,
+					'Expression',
+					'FunctionProperty',
+				],
+				(name) => funcPropertyName1 ?? name,
+			)
+			.updateIn(
+				[
+					'contextDescription',
+					0,
+					'context',
+					0,
+					'KeyItemDeclaration',
+					'Expression',
+					'FunctionDeclaration',
+					'Arguments',
+					1,
+					'Expression',
+					'FunctionProperty',
+				],
+				(name) => funcPropertyName2 ?? name,
+			);
+		return updated.toJS();
+	} else return base;
+};
 
 export const functionsFixtures = {
 	expression,
