d9311035c62d21f93f5e9506922f26820da4bb7f
Алексей Гераим <alexeygeraim@Aleksejs-MacBook-Air.local>
2024-06-21 13:23:54 +0300

    chore: add codecov

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,6 +13,17 @@ export default defineConfig({
 			reportsDirectory: './coverage',
 			provider: 'istanbul',
 			reporter: ['text', 'json-summary', 'json', 'html-spa'],
+			exclude: [
+				'vitepress',
+				'*/fixtures',
+				// All ts files that only contain types, due to ALL
+				'/*.{interface,type,d}.ts',
+				'/{interfaces,types}/*.ts',
+				// All index files that *should* only contain exports from other files
+				'/index.{js,ts}',
+				// All exports files that make subpackages available as submodules
+				'**/exports/*.{js,ts}',
+			],
 		},
 	},
 });
