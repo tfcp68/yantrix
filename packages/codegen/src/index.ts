07d9260215a879d6929b1da9083d246ff2ed22a7
Alexey Geraim <oblivion25565@gmail.com>
2024-06-25 00:11:31 +0300

    chore: exclude yantrix parser

--- packages/codegen/src/index.ts
+++ packages/codegen/src/index.ts
@@ -2,20 +2,18 @@ import { IGenerateOptions, TStateIncludingNotes } from './types/common.js';
 import { fmt } from './utils/utils.js';
 import { CodegenCreator } from './core/Codegen.js';
 import { TStateDiagramMatrix } from '@yantrix/mermaid-parser';
-import { YantrixParser } from '@yantrix/yantrix-parser';
 
 export * from './types/common.js';
 export * from './core/modules/index.js';
 
 export const generateAutomataFromStateDiagram = async (diagram: TStateDiagramMatrix, options: IGenerateOptions) => {
 	const { states, transitions } = diagram;
-	const parserInstance = new YantrixParser();
 
 	const statesIncludingNotes = states.map((state) => {
 		const input = state.notes.flatMap((e) => e.join('\n')).join(' ');
 		if (input === '') return { ...state, notes: null };
 
-		return { ...state, notes: parserInstance.parse(input) } as TStateIncludingNotes;
+		return { ...state, notes: input } as unknown as TStateIncludingNotes;
 	});
 
 	const creator = new CodegenCreator({
