7803380e6f64b4cc5897c37f1e0a8da4811f0ea8
Sin <shulgin.grigoriy@yandex.ru>
2024-07-02 22:11:07 +0300

    chore(docs): add-code-to-mermaid (#90)

---
+++ vitepress/.vitepress/theme/components/Mermaid/CopyButton.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<CopySVG fill="none" @click="copy" v-if="!isCopied" />
+		<CopyCopiedSVG v-if="isCopied" />
+	</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import CopySVG from '../../svgs/CopySVG.vue';
+import CopyCopiedSVG from '../../svgs/CopyCopiedSVG.vue';
+
+const props = defineProps({
+	textToCopy: String,
+});
+
+const isCopied = ref(false);
+const copy = () => {
+	navigator.clipboard.writeText(props.textToCopy);
+
+	isCopied.value = true;
+
+	setTimeout(() => {
+		isCopied.value = false;
+	}, 2000);
+};
+</script>
