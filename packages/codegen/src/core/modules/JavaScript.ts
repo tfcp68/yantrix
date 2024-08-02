7b0e0d7c71b861cda8e0c24a1103d7c0c313c49e
Alexey Geraim <aleksey.geraim@restream.ru>
2024-08-02 17:48:20 +0300

    feat(codegen): raw expression

--- packages/codegen/src/core/modules/JavaScript.ts
+++ packages/codegen/src/core/modules/JavaScript.ts
@@ -49,8 +49,7 @@ export const Expressions: TExpressionRecord = {
 							res.push(`${getDefaultPropertyContext(path, identifier)}`);
 						}
 					} else {
-						const { expressionType } = item;
-						if (expressionType === ExpressionTypes.Function) {
+						if (item.expressionType === ExpressionTypes.Function) {
 							// @ts-ignore
 							res.push(recursive(item));
 						} else {
@@ -290,7 +289,7 @@ export class JavaScriptCodegen implements ICodegen {
 
 						if (isKeyItemWithExpression(keyItem)) {
 							const { expression } = keyItem;
-
+							console.log(keyItem);
 							// @ts-ignore
 							const expressionValueRight = Expressions[expression.expressionType](expression);
 
@@ -301,7 +300,7 @@ export class JavaScriptCodegen implements ICodegen {
 					} else {
 						const { expression } = keyItem;
 
-						//@ts-check
+						// @ts-ignore
 						const expressionValueRight = Expressions[expression.expressionType](expression);
 						return `(function(){
 						return ${expressionValueRight}
