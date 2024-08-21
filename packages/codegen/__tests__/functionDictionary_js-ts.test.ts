1c4beeab0ae552f1a90c8d40fd697984985f7133
Denis Perevertov <arrow8086527denis@gmail.com>
2024-08-20 23:59:42 -0400

    fix: small test changes

--- packages/codegen/__tests__/functionDictionary_js-ts.test.ts
+++ packages/codegen/__tests__/functionDictionary_js-ts.test.ts
@@ -31,105 +31,82 @@ describe('JS/TS Function Dictionary', () => {
 		functionDictionaryFixture = new FunctionDictionary(builtInFunctions);
 	});
 
-	describe('Dictionary is not empty and has built-in functions inside upon creation', () => {
-		test('', () => {
-			assert.isNotNull(functionDictionary);
-			assert.deepEqual(functionDictionary, functionDictionaryFixture);
-		});
+	test('Dictionary is not empty and has built-in functions inside upon creation', () => {
+		assert.isNotNull(functionDictionary);
+		assert.deepOwnInclude(functionDictionary, functionDictionaryFixture);
 	});
 
 	describe('Can get functions from a dictionary', () => {
-		describe('Built-in functions', () => {
+		test('Built-in functions', () => {
 			Object.keys(builtInFunctions).forEach((key) => {
-				test(`Built-in function: ${key}`, () => {
-					const expected = builtInFunctions[key as keyof typeof builtInFunctions];
-					const func = functionDictionary.get(key);
-					expect(func).toEqual(expected);
-				});
+				const expected = builtInFunctions[key as keyof typeof builtInFunctions];
+				const func = functionDictionary.get(key);
+				expect(func).toEqual(expected);
 			});
 		});
-		describe('Custom functions', () => {
+		test('Custom functions', () => {
 			testFunctionsExamples.forEach((f) => {
 				const key = randomString(20);
-				test(`${key}`, () => {
-					functionDictionary.register(key, f);
-					expect(functionDictionary.get(key)).toBeTruthy();
-				});
+				functionDictionary.register(key, f);
+				expect(functionDictionary.get(key)).toEqual(f);
 			});
 		});
 	});
 
-	describe('Cannot get a function under a non-existing key', () => {
+	test('Cannot get a function under a non-existing key', () => {
 		for (let i = 0; i < 20; i++) {
 			const randomKey = randomString(20);
-			test(`${randomKey}`, () => {
-				expect(() => functionDictionary.get(randomKey)).toThrowError();
-			});
+			expect(() => functionDictionary.get(randomKey)).toThrowError();
 		}
 	});
 
-	describe('Can register custom functions inside of a dictionary', () => {
+	test('Can register custom functions inside of a dictionary', () => {
 		testFunctionsExamples.forEach((f) => {
 			const functionName = randomString();
-			test(`${functionName}: ${f}`, () => {
-				functionDictionary.register(functionName, f);
-				expect(functionDictionary.get(functionName)).toEqual(f);
-			});
+			functionDictionary.register(functionName, f);
+			expect(functionDictionary.get(functionName)).toEqual(f);
 		});
 	});
 
-	describe('Custom functions return the same result before and after being added to a dictionary', () => {
+	test('Custom functions return the same result before and after being added to a dictionary', () => {
 		const customFunction = (x: number) => add(mult(x, 2), pow(x, 3));
 		for (let i = 0; i < 20; i++) {
 			const num = randomInteger(1, 15);
 			const expectedResult = customFunction(num);
 
 			const customKey = randomString(10);
-			functionDictionary.register(customKey, customFunction);
 
-			test(`${customFunction}, expected result ${expectedResult}`, () => {
-				const functionFromDictionary = functionDictionary.get(customKey)!;
-				expect(functionFromDictionary(num)).toEqual(expectedResult);
-			});
+			const functionFromDictionary = functionDictionary.register(customKey, customFunction)!;
+			expect(functionFromDictionary(num)).toEqual(expectedResult);
 		}
 	});
 
-	describe('Cannot register a function with incorrect key length', () => {
+	test('Cannot register a function with incorrect key length', () => {
 		for (let i = 0; i < 20; i++) {
 			const invalidKey = randomString(randomInteger(256, 1000));
-			test(`${invalidKey} - ERROR`, () => {
-				expect(() => functionDictionary.register(invalidKey, testFunctionsExamples[0] ?? null)).toThrowError();
-			});
+			expect(() => functionDictionary.register(invalidKey, testFunctionsExamples[0] ?? null)).toThrowError();
 		}
 	});
 
-	describe('Cannot register a function with incorrect format', () => {
+	test('Cannot register a function with incorrect key format', () => {
 		const customFunction = testFunctionsExamples[0] ?? null;
 		invalidFunctionNamesTemplates.forEach((temp) => {
 			const invalidName = createNameFromTemplate(temp);
-			test(`${invalidName}`, () => {
-				expect(() => functionDictionary.register(invalidName, customFunction)).toThrowError();
-			});
+			expect(() => functionDictionary.register(invalidName, customFunction)).toThrowError();
 		});
 	});
 
 	describe('Cannot register a function under an already existing key', () => {
-		describe('Built-in keys', () => {
+		test('Built-in keys', () => {
 			Object.keys(builtInFunctions).forEach((key) => {
-				test(`${key}`, () => {
-					expect(() => functionDictionary.register(key, testFunctionsExamples[0] ?? null)).toThrowError();
-				});
+				expect(() => functionDictionary.register(key, testFunctionsExamples[0] ?? null)).toThrowError();
 			});
 		});
-		describe('Custom key', () => {
+		test('Custom key', () => {
 			for (let i = 0; i < 20; i++) {
 				const customKey = randomString(10);
-				test(`${customKey}`, () => {
-					functionDictionary.register(customKey, testFunctionsExamples[0] ?? null);
-					expect(() =>
-						functionDictionary.register(customKey, testFunctionsExamples[0] ?? null),
-					).toThrowError();
-				});
+				functionDictionary.register(customKey, testFunctionsExamples[0] ?? null);
+				expect(() => functionDictionary.register(customKey, testFunctionsExamples[0] ?? null)).toThrowError();
 			}
 		});
 	});
