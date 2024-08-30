8a683e546be110a568b863e7e65c5681249e1a2a
Noktomezo <noktomezo@duck.com>
2024-08-30 05:51:22 +0300

    fix: reverts

--- packages/codegen/__tests__/functionDictionary_js-ts.test.ts
+++ packages/codegen/__tests__/functionDictionary_js-ts.test.ts
@@ -5,7 +5,7 @@ import { randomInteger, randomString } from '@yantrix/utils';
 import { SpecialCharList } from '@yantrix/yantrix-parser';
 import { FunctionDictionary, TAutomataFunction } from '@yantrix/automata';
 
-import { builtInFunctions } from '..';
+import { builtInFunctions } from '@yantrix/codegen';
 import { saveAndGenerate } from './fixtures/utils.js';
 
 let functionDictionaryFixture: FunctionDictionary;
