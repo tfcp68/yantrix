bf3f33b32de70547e35c0f8f07f9a2510d331e05
nekitech <nekitech-x@mail.ru>
2024-09-13 09:42:14 +0300

    refactor: slice

--- packages/redux/src/index.ts
+++ packages/redux/src/index.ts
@@ -1,9 +1,7 @@
-import { Dispatch, combineReducers, configureStore } from '@reduxjs/toolkit';
+import { Dispatch } from '@reduxjs/toolkit';
 import { GenericAutomata, TAutomataStateContext } from '@yantrix/automata';
 import { uniqId } from '@yantrix/utils';
-import GamePhaseTest from '../__tests__/fixtures/GamePhaseAutomataTest';
-import { TActionGenerator, TAutomataId, TReduxConnectedAutomata, TStateFSMSlice } from './types.js';
-import { createFSMSlice } from './slice/slice';
+import { TActionGenerator, TAutomataId, TReduxConnectedAutomata } from './types.js';
 
 export * from './slice/slice.js';
 export * from './types.js';
@@ -57,27 +55,4 @@ export function useReduxAutomata(automataId: TAutomataId) {
 		throw new Error(`Automata ${automataId} not found`);
 	}
 	return [automata.basicAutomata, automata.dispatch] as const;
-};
-
-const { actions, name, reducer, selectors } = createFSMSlice({
-	name: GamePhaseTest.id,
-	Fsm: GamePhaseTest,
-	contextToRedux: (context: any) => ({
-		counter: context.counter,
-		redColorOn: ['Red', 'RedYellow'].includes(context.state),
-		yellowColorOn: ['Yellow', 'RedYellow'].includes(context.state),
-		greenColorOn: ['Green'].includes(context.state),
-	}),
-	selectors: {
-		state: (sliceState): TStateFSMSlice['state'] => sliceState.state,
-		context: (sliceState): TStateFSMSlice['context'] => sliceState.context,
-	},
-});
-
-const store = configureStore({
-	reducer: combineReducers({
-		[name]: reducer,
-	}),
-});
-console.log(actions, selectors);
-store.dispatch(actions.RESET({}));
+}
