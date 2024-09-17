0ffd0b32c39973888b523476b0a2cdd364bf789c
Alexey Geraim <oblivion25565@gmail.com>
2024-09-17 20:39:22 +0300

    feat(codegen-tests): add package codegen-tests

---
+++ packages/codegen-tests/src/fixtures/utils.ts
@@ -0,0 +1,80 @@
+import { exec } from 'node:child_process';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { generateAutomataFromStateDiagram, TOutLang } from '@yantrix/codegen';
+import { createStateDiagram, parseStateDiagram } from '@yantrix/mermaid-parser';
+import { ensureDir } from 'fs-extra';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const pathSave = path.resolve(dirname);
+const langToExt: Record<TOutLang, string> = {
+	JavaScript: 'js',
+	TypeScript: 'ts',
+	Java: 'java',
+	Python: 'py',
+};
+
+type TGenerateAutomataParams = {
+	input: string;
+	lang: TOutLang;
+	automataName: string;
+	constants?: string;
+};
+
+export function saveFile(fileName: string, content: string, ext: string) {
+	exec('yantrix codegen ./src/diagrams/traffic-light.mermaid -l TypeScript -o src/generated/TrafficLightAutomata.ts -c TrafficLightAutomata');
+	ensureDir(path.resolve(pathSave, `generated`));
+	fs.writeFileSync(path.resolve(pathSave, `generated/${fileName}_generated.${ext}`), content);
+}
+
+export async function generateAutomata(options: TGenerateAutomataParams) {
+	const stateDiagramStructure = await parseStateDiagram(options.input);
+	const stateDiagram = await createStateDiagram(stateDiagramStructure);
+
+	const generatedAutomataOutput = await generateAutomataFromStateDiagram(stateDiagram, {
+		className: options.automataName,
+		outLang: options.lang,
+		constants: options.constants!,
+	});
+
+	return generatedAutomataOutput;
+}
+
+export async function generateAndSave(options: TGenerateAutomataParams, fileName: string) {
+	const ext = langToExt[options.lang];
+	const automata = await generateAutomata(options);
+
+	saveFile(fileName, automata, ext);
+}
+
+export function mapFromObjectToString(a: Record<string, any>, startSymbol: string = '') {
+	return Object
+		.entries(a)
+		.map(([key, value]) => {
+			if (value === null) {
+				return `${startSymbol}${key}`;
+			}
+			if (typeof value === 'string') return `${startSymbol}${key}="${value}"`;
+
+			return `${startSymbol}${key}=${value}`;
+		})
+		.join(',');
+}
+
+export function mapFromStringToObject(a: string) {
+	return a.split(',').reduce((acc, el) => {
+		const [key, value] = el.split('=');
+		acc[key as keyof typeof acc] = value || null;
+		return acc;
+	}, Object.create(null));
+}
+
+export function objectKeysToString(obj: Record<string, any>, startSymbol: string = '') {
+	return Object
+		.keys(obj)
+		.map(el => `${startSymbol}${el}`)
+		.join(',');
+}
