9c7878d4056db2668321659e918acc399eceb2a6
Noktomezo <noktomezo@duck.com>
2024-09-18 20:35:04 +0300

    chore: add polyfills

--- examples/01-traffic-light/vite.config.ts
+++ examples/01-traffic-light/vite.config.ts
@@ -1,9 +1,21 @@
 import path from 'node:path';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		nodePolyfills({
+			protocolImports: true,
+			include: ['process', 'path', 'url'],
+		}),
+	],
+	build: {
+		rollupOptions: {
+			external: ['vite-plugin-node-polyfills/shims/process'],
+		},
+	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
