f9cae263974ff21d1d42f22210d0d8e2a9662036
Alexey Geraim <oblivion25565@gmail.com>
2024-09-26 10:26:46 +0300

    docs: example iframe styles

--- vitepress/.vitepress/theme/index.js
+++ vitepress/.vitepress/theme/index.js
@@ -1,5 +1,7 @@
 import DefaultTheme from 'vitepress/theme';
 import Mermaid from './components/Mermaid/Mermaid.vue';
+import './custom.css';
+
 export default {
 	...DefaultTheme,
 	enhanceApp({ app }) {
