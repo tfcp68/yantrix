9919fccd661a714be39f238490adfe348fc4555a
Denis Perevertov <arrow8086527denis@gmail.com>
2024-11-22 03:18:02 -0500

    refactor: createEventBus serializer

--- packages/codegen/src/core/modules/JavaScript/JavaScriptCompiler/events/serializer.ts
+++ packages/codegen/src/core/modules/JavaScript/JavaScriptCompiler/events/serializer.ts
@@ -17,53 +17,60 @@ function getEventAdapterCode(props: {
 }
 
 function getCreateEventBusFunctionCode() {
-	return `export function createEventBus(id, FSMs) {
+	const lines: string[] = [];
+	lines.push('export function createEventBus(id, FSMs) {');
+	lines.push('const EventBus = new BasicEventBus();');
+	lines.push('EventBus.correlationId = id;');
+	lines.push(`const automatas = ${getEventBusAutomatasCode()}`);
+	lines.push(getEventBusSubscriptions());
+	lines.push('return [EventBus, automatas];');
+	lines.push('}');
+	return lines.join('\n');
+}
+
+function getEventBusAutomatasCode() {
+	return `Object.fromEntries(
+		Object.entries(FSMs).map(([automataId, AutomataClass]) => {
+			return [automataId, new AutomataClass()]
+		})
+	);`;
+}
+
+function getEventBusSubscriptions() {
+	return `Object.entries(GlobalEventDictionary.getDictionary())
+	.forEach(([eventName, eventId]) => {
 	
-		const EventBus = new BasicEventBus();
-		EventBus.correlationId = id;
-	
-		const automatas = Object.fromEntries(
-			Object.entries(FSMs).map(([automataId, AutomataClass]) => {
-				return [automataId, new AutomataClass()]
-			})
-		);
-	
-		Object.entries(GlobalEventDictionary.getDictionary()).forEach(([eventName, eventId]) => {
-	
-			EventBus.subscribe(eventId, ({ event, meta }) => {
-	
-				const nextEventsToProcess = [];
-	
-				Object.values(automatas).forEach(automata => {
-					const newActions = automata.eventAdapter?.handleEvent({ event, meta }) ?? [];
-					for (const action of newActions) {
-						automata.dispatch(action);
-						const newAutomataEvents = automata.eventAdapter?.handleTransition(automata.getContext()) ?? [];
-						nextEventsToProcess.push(...newAutomataEvents);
-					}
-				})
-	
-				EventBus.dispatch(...nextEventsToProcess);
-		
-				return {
-					event,
-					meta,
-					task_id: \`event_\${eventName}_\${eventId}\`,
-					result: new Promise((resolve, reject) => {
-						try {
-							const eventStack = EventBus.getEventStack();
-							resolve(eventStack);
-						} catch {
-							reject(new Error('Error getting event stack'))
-						}
-					})
+		EventBus.subscribe(eventId, ({ event, meta }) => {
+
+			const nextEventsToProcess = [];
+
+			Object.values(automatas).forEach(automata => {
+				const newActions = automata.eventAdapter?.handleEvent({ event, meta }) ?? [];
+				for (const action of newActions) {
+					automata.dispatch(action);
+					const newAutomataEvents = automata.eventAdapter?.handleTransition(automata.getContext()) ?? [];
+					nextEventsToProcess.push(...newAutomataEvents);
 				}
 			})
+
+			EventBus.dispatch(...nextEventsToProcess);
 	
+			return {
+				event,
+				meta,
+				task_id: \`event_\${eventName}_\${eventId}\`,
+				result: new Promise((resolve, reject) => {
+					try {
+						const eventStack = EventBus.getEventStack();
+						resolve(eventStack);
+					} catch {
+						reject(new Error('Error getting event stack'))
+					}
+				})
+			}
 		})
-	
-		return [EventBus, automatas];
-	}`;
+
+	});`;
 }
 
 export const eventsSerializer = {
