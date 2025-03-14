b829e8bf8c939530c852845be9e2b53d348c0bf4
Alexey Geraim <aleksey.geraim@restream.ru>
2025-03-14 12:06:37 +0300

    feat(inject): remove transpile

--- packages/codegen/src/core/InjectFunctionsProcess.ts
+++ packages/codegen/src/core/InjectFunctionsProcess.ts
@@ -1,7 +1,5 @@
-import { readFile, unlink, writeFile } from 'node:fs/promises';
 import path from 'node:path';
 import { TNullable } from '@yantrix/utils';
-import ts from 'typescript';
 import { TOutLang, TUserFunctionsDict } from '../types/common';
 import { ModuleNames } from './modules';
 
@@ -13,26 +11,8 @@ const processFileTS = async (filePath: TNullable<string>): Promise<TUserFunction
 		throw new Error('Only .js or .ts files are supported');
 	}
 
-	if (fileExtension === '.js') {
-		return await import(filePath);
-	}
-
-	const fileContent = await readFile(filePath, 'utf8');
-
-	const result = ts.transpileModule(fileContent, {
-		compilerOptions: {
-			module: ts.ModuleKind.CommonJS,
-			target: ts.ScriptTarget.ES2015,
-		},
-	});
-
-	const tempJsPath = path.join(path.dirname(filePath), '_temp.js');
-	await writeFile(tempJsPath, result.outputText);
-
-	const functions = await import(tempJsPath);
-	await unlink(tempJsPath);
-
-	return functions;
+	const functions = await import(filePath);
+	return functions.default;
 };
 
 const processFileDict: Record<TOutLang, (filePath: TNullable<string>) => Promise<TUserFunctionsDict>> = {
