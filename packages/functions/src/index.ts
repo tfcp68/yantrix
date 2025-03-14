8dc6580ce219728213116169e1c3427cc51be55f
Noktomezo <noktomezo@duck.com>
2025-03-14 05:32:25 +0300

    chore: remove unnecessaries

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,9 +1,6 @@
 import * as conditionals from './conditionals';
 import * as predicates from './predicates';
 import * as transformers from './transformers';
-import { isIterable } from './types/guards';
-
-console.log(isIterable('awdawwa'));
 
 export const builtInFunctions = {
 	...conditionals,
