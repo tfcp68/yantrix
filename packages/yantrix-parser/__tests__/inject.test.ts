080f989365dc42a80e5de80a945aa1cb667b9d1a
Alexey Geraim <aleksey.geraim@restream.ru>
2025-03-28 02:49:00 +0300

    feat(yantrix-parser): more tests inject

--- packages/yantrix-parser/__tests__/inject.test.ts
+++ packages/yantrix-parser/__tests__/inject.test.ts
@@ -4,12 +4,26 @@ import { YantrixParser } from '../src';
 describe('inject', () => {
 	it('grammar support inject', () => {
 		const parser = new YantrixParser();
-		const input = `inject/test`;
+		const input = `inject/isBoolean`;
 		const output = parser.parse(input);
 
 		expect(output.inject.length).equal(1);
 		expect(output.inject[0]).toStrictEqual({
-			identifier: 'test',
+			identifier: 'isBoolean',
 		});
+	});
+
+	it('should reject invalid inject directive syntax', () => {
+		const parser = new YantrixParser();
+		const input = `inject isBoolean`;
+
+		expect(() => parser.parse(input)).toThrow();
+	});
+
+	it('should reject inject directive with empty identifier', () => {
+		const parser = new YantrixParser();
+		const input = `inject/`;
+
+		expect(() => parser.parse(input)).toThrow();
 	});
 });
