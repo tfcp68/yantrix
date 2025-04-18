d71bc5a2c193f227b38617865853b63b77670c64
Denis Perevertov <regicuinte@gmail.com>
2025-04-18 03:15:13 -0400

    test: epoch function tests

--- packages/codegen-tests/src/automataInternals.test.ts
+++ packages/codegen-tests/src/automataInternals.test.ts
@@ -20,18 +20,18 @@ end note
 `;
 
 describe('automata internal functions tests', async () => {
-	await generateAndSave({ input, automataName: 'AutomataInternalsTest', lang: ModuleNames.JavaScript }, 'functions_automata_internals');
+	await generateAndSave({ input, automataName: 'AutomataInternalsTest', lang: ModuleNames.JavaScript }, 'functions_automataInternals');
 
-	const { AutomataInternalsTest, statesDictionary, actionsDictionary, epoch } = await import(`./fixtures/generated/functions_automata_internals_generated.js`);
+	const { AutomataInternalsTest, statesDictionary, actionsDictionary } = await import(`./fixtures/generated/functions_automataInternals_generated.js`);
 
 	let automata: AutomataInternalsTest;
 
+	// alternatively, can use references in automata's function dictionary, but I'd test the functions separately from the dictionary
 	const currentStateIdFunction = internalFunctions.currentStateId(AutomataInternalsTest);
 	const currentStateNameFunction = internalFunctions.currentStateName(AutomataInternalsTest, statesDictionary);
 	const currentActionIdFunction = internalFunctions.currentActionId(AutomataInternalsTest);
 	const currentActionNameFunction = internalFunctions.currentActionName(AutomataInternalsTest, actionsDictionary);
 	const currentCycleFunction = internalFunctions.currentCycle(AutomataInternalsTest);
-	const currentEpochFunction = internalFunctions.currentEpoch(epoch);
 
 	beforeEach(() => {
 		automata = new AutomataInternalsTest();
@@ -95,16 +95,22 @@ describe('automata internal functions tests', async () => {
 		});
 	});
 
-	// skip for now
-	describe.skip('currentEpoch functions test', () => {
+	describe('currentEpoch functions test', async () => {
+
+		// generating a separate bundle for the epoch test
+		await generateAndSave({ input, automataName: 'AutomataInternalsEpochTest', lang: ModuleNames.JavaScript }, 'functions_automataInternalsEpoch');
+		const { AutomataInternalsEpochTest, actionsDictionary, functionDictionary } = await import(`./fixtures/generated/functions_automataInternalsEpoch_generated.js`);
+		const automata = new AutomataInternalsEpochTest();
+		const currentEpochFunction = functionDictionary.get('currentEpoch')!;
+
 		it('initial epoch is 1', () => {
-			const Epoch = currentEpochFunction();
-			expect(Epoch).toBe(1);
+			const currentEpoch = currentEpochFunction();
+			expect(currentEpoch.val).toBe(1);
 		});
-		it('epoch is incremented after action dispatch of some automata', () => {
+		it('epoch is incremented after action dispatch of some automata', async () => {
 			automata.dispatch({ action: actionsDictionary.Switch, payload: {} });
-			const Epoch = currentEpochFunction();
-			expect(Epoch).toBe(2);
+			const currentEpoch = currentEpochFunction();
+			expect(currentEpoch.val).toBe(2);
 		});
 	});
 });
