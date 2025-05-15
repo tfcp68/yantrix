60f39c3e81637af7eaf786c313cde50a12121e05
Noktomezo <noktomezo@duck.com>
2025-05-15 22:52:57 +0300

    chore: replace funcs with pydash ones

--- packages/functions/src/__init__.py
+++ packages/functions/src/__init__.py
@@ -1,44 +1,57 @@
-"""Exports built-in Yantrix functions implemented in Python."""
+from .python.transformers import (
+    add, diff, mult, div, pow_, inc, dec, neg, inv,
+    mod, trunc, ceil, round_, sin, cos, sqrt,
+    log, ln, lg, deg, rad,
+    len_, left, right, index_of, shuffle, reverse,
+    sort, lookup, repeat, substr, filter_by, pluck
+)
 
-from .python.arithmetic import ARITHMETIC_FUNCTIONS
-from .python.special_math import SPECIAL_MATH_FUNCTIONS
-from .python.statistics import STATISTICS_FUNCTIONS
-from .python.list_transformers import LIST_TRANSFORMERS
-from .python.string_transformers import STRING_TRANSFORMERS
-from .python.object_transformers import OBJECT_TRANSFORMERS
-from .python.collection_transformers import COLLECTION_TRANSFORMERS
-from .python.conditionals import CONDITIONAL_FUNCTIONS
-from .python.predicates import PREDICATE_FUNCTIONS
+from .python.conditionals import (
+    _if, _case, coalesce, random
+)
 
-# Combine all function dictionaries into a single master dictionary
-# This allows looking up any built-in function by its name.
+from .python.predicates import (
+    and_, all_, or_, any_, not_, none,
+    is_even, is_odd, is_integer,
+    is_greater, is_greater_or_equal,
+    is_less, is_less_or_equal,
+    is_negative, is_positive,
+    contains, has,
+    is_equal, is_none
+)
+
 BUILT_IN_FUNCTIONS = {
-    **ARITHMETIC_FUNCTIONS,
-    **SPECIAL_MATH_FUNCTIONS,
-    **STATISTICS_FUNCTIONS,
-    **LIST_TRANSFORMERS,
-    **STRING_TRANSFORMERS,
-    **OBJECT_TRANSFORMERS,
-    **COLLECTION_TRANSFORMERS,
-    **CONDITIONAL_FUNCTIONS,
-    **PREDICATE_FUNCTIONS,
+    # Arithmetic functions
+    'add': add, 'diff': diff, 'mult': mult, 'div': div,
+    'pow_': pow_, 'inc': inc, 'dec': dec, 'neg': neg,
+    'inv': inv, 'mod': mod, 'trunc': trunc, 'ceil': ceil,
+    'round_': round_,
+
+    # Special math functions
+    'sin': sin, 'cos': cos, 'sqrt': sqrt, 'log': log,
+    'ln': ln, 'lg': lg, 'deg': deg, 'rad': rad,
+
+    # List/String transformers
+    'len_': len_, 'left': left, 'right': right,
+    'index_of': index_of, 'shuffle': shuffle, 'reverse': reverse,
+
+    # Collection transformers
+    'sort': sort, 'lookup': lookup, 'repeat': repeat,
+    'substr': substr, 'filter_by': filter_by, 'pluck': pluck,
+
+    # Conditionals
+    '_if': _if, '_case': _case, 'coalesce': coalesce, 'random': random,
+
+    # Predicates
+    'and_': and_, 'all_': all_, 'or_': or_, 'any_': any_,
+    'not_': not_, 'none': none,
+    'is_even': is_even, 'is_odd': is_odd, 'is_integer': is_integer,
+    'is_greater': is_greater, 'is_greater_or_equal': is_greater_or_equal,
+    'is_less': is_less, 'is_less_or_equal': is_less_or_equal,
+    'is_negative': is_negative, 'is_positive': is_positive,
+    'contains': contains, 'has': has,
+    'is_equal': is_equal, 'is_none': is_none
 }
 
-# Expose individual functions or categories if needed, e.g.:
-# from .python.arithmetic import add, mult
-# from .python.predicates import is_even, contains
-
-__all__ = [
-    'ARITHMETIC_FUNCTIONS',
-    'SPECIAL_MATH_FUNCTIONS',
-    'STATISTICS_FUNCTIONS',
-    'LIST_TRANSFORMERS',
-    'STRING_TRANSFORMERS',
-    'OBJECT_TRANSFORMERS',
-    'COLLECTION_TRANSFORMERS',
-    'CONDITIONAL_FUNCTIONS',
-    'PREDICATE_FUNCTIONS',
-    'BUILT_IN_FUNCTIONS',
-    # Add individual function names here if you want them directly importable
-    # e.g., 'add', 'is_even' after importing them above
-]
+# Expose all functions directly
+__all__ = list(BUILT_IN_FUNCTIONS.keys())
